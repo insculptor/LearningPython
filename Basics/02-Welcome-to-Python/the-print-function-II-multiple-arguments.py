f413235ae329875b978767077b55235dfdbefe67
Ravi DHIR <er.dr.rd@gmail.com>
2020-07-28 21:53:16 +0530

    print

---
+++ Basics/02-Welcome-to-Python/the-print-function-II-multiple-arguments.py
@@ -0,0 +1,23 @@
+## Print function can take ANY number of sequential values, seperated by ,
+
+print("3", 4, "B")
+# Note that every output is seperated by SPACE
+
+print("AA",55,66,77,88,99,"F")
+
+print(5+3 , 10-9)
+
+
+# Shotcut to COPY LINE AND PASTE IN NEXT LINE
+## SHIFT + ALT + DOWN
+## SHIFT + ALT + DOWN
+
+print("Hello", 10, 3.14)
+
+
+
+
+
+
+
+
---
+++ Basics/02-Welcome-to-Python/the-print-function-III-parameters-and-arguments.py
@@ -0,0 +1,18 @@
+# Parameter: A Name for an expected argument to a function.
+
+# sep - Seperator - default - Space
+print("3",4,5,5,6,sep="_")
+print("3",4,5,5,6,sep="_6")
+print("3",4,5,5,6,sep="--*--")
+
+
+# end - 
+print("Hello","Bye", end='**')
+print("Again","Bye",)
+
+# Since they are named, order output doesnt matter
+print("A","B","C",end="!!",sep="_")
+
+# file
+
+# flush
