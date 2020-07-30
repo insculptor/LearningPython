d6838535615bf178b4dd1bb0bf8302167a6458ea
Ravi DHIR <er.dr.rd@gmail.com>
2020-07-30 14:32:45 +0530

    booleans

---
+++ Basics/03-Numbers-Booleans-and-Equality/the-boolean-data-type.py
@@ -0,0 +1,25 @@
+# Either True or False
+print(True)
+print(False)
+# Without Double Quotes, are not strings
+# Boolean is a dataTye that we can get after evaluating an expression
+
+if 5==5:
+    print("It's true")
+else:
+    print("False it is")
+
+
+print(5==6)
+print(5==5)
+
+print("ravi" == 'ravi')
+print("ravi" == 'Ravi')
+
+print('#'*50)
+
+print(5 == 5.0) #True
+
+print((5==5.1)==True)
+print((5==5.1)==False)
+print((5!=5.1)==True)
